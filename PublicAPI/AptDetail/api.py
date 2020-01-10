d169e40f2868cc33de10743ef317183bacc653e0
dk-lite <kdk5go@gmail.com>
2020-01-10 17:37:55 +0900

    Api update

--- PublicAPI/AptDetail/api.py
+++ PublicAPI/AptDetail/api.py
@@ -8,7 +8,7 @@ from urllib.request import urlopen, Request
 class AptDetailReader:
 
     URL = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
-
+    NUM_OF_ROWS = "10000"
     def __init__(self, service_key):
         self.service_key = service_key
     
@@ -17,19 +17,36 @@ class AptDetailReader:
         queryParams = '?' + urlencode({ \
             quote_plus('LAWD_CD') : LAWD_CD,
             quote_plus('DEAL_YMD') : DEAL_YMD,
+            quote_plus('numOfRows') : self.NUM_OF_ROWS,
             })
-        request = Request(url + queryParams + self.service_key)
+
+        request = Request(self.URL + queryParams + self.service_key)
         request.get_method = lambda: 'GET'
         response_body = urlopen(request).read().decode('utf8')
         
-        return __json2pd(xmltodict.parse(response_body))
+        return self.__json2pd(xmltodict.parse(response_body))
 
     def __json2pd(self, json_data):
-        pass
+
+        items = json_data['response']['body']['items']
+        if items is None: 
+            return pd.DataFrame()
+
+        datas = items['item']
+        output = pd.DataFrame(columns=datas[0].keys())
+        
+        #pd.concat([ pd.DataFrame(data, columns=datas[0].keys()) for data in datas ], ignore_index=True)
+        
+        for data in datas :
+            output = output.append(data, ignore_index=True)
+
+        return output
+
 
 def main():
     apt = AptDetailReader("&ServiceKey=aKVcsSF8UlpRkppSsPS38IZO8UZ87fNBzMTtrrWtoadmn7ySBos%2BX8AOc6M%2F47Temd2cIbpl4%2BFxq%2Btu0KoKWA%3D%3D")
     df_data = apt.DataReader("41135", "201911")
+    print(df_data.head())
 
 if __name__ == "__main__":
     main()
