b59324f72f3352ac5f0f949aca8028dbd3c79abe
dk-lite <kdk5go@gmail.com>
2020-03-18 22:52:33 +0900

    collector

---
+++ collector/collector.py
@@ -0,0 +1,53 @@
+import requests, os
+import json, argparse, xmltodict
+import pandas
+from Loader import Loader
+from AptDetail import *
+from datetime import datetime, timedelta
+
+def main():
+
+    dt_index = pandas.date_range(start='20160101', end='20191201')
+    dt_list = dt_index.strftime("%Y%m").tolist()
+    dt_list = list(set(dt_list))
+    dt_list.sort(reverse=True)
+
+    abs_path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(abs_path,"data")
+
+    file_list = os.listdir(path)
+    file_list_json = [ file for file in file_list if file.endswith(".json")]
+
+    for dt in dt_list:
+        if dt +'.json' not in file_list_json:
+            cur_date = dt
+            break
+
+    print(cur_date)
+    PATH = os.path.join(abs_path,'data/'+str(cur_date)+'.json')
+
+    # argument 
+    arg = argparse.ArgumentParser()
+    arg.add_argument('--date', type=str, default=cur_date)
+    args=arg.parse_args()
+    
+
+    # Loader 
+    codes = Loader.get_codes()
+    configs = Loader.get_configs()
+
+    #data set
+    apt = AptDetailReader(configs['service_key'])
+
+    result = []
+    for code in codes:
+       items = apt.DataReader(code, args.date)
+       if items is None: continue
+       result += items
+       break
+       
+    with open(PATH, "w", encoding="utf-8") as make_file:
+        json.dump(result, make_file, ensure_ascii=False, indent="\t")
+
+if __name__ == "__main__":
+    main()
--- collector/run.py
+++ collector/run.py
@@ -9,7 +9,11 @@ from datetime import datetime, timedelta
 # json_str = dumps(record)
 # record2 = loads(json_str)
 
+
+
 def main():
+
+    PATH = './data/'
 
     now = datetime.now() - timedelta(days=15)
     cur_date = now.strftime('%Y%m') 
@@ -26,12 +30,20 @@ def main():
 
     #data set
     apt = AptDetailReader(configs['service_key'])
+
+    result = []
     for code in codes:
        items = apt.DataReader(code, args.date)
        if items is None: continue
+
+       result.append(items)
+       
        for item in items:
            requests.post("http://localhost:3691/data-lake/apt-trade-info", data=item)
 	
+    with open(PATH+cur_date+".json", "w", encoding="utf-8") as make_file:
+        json.dump(result, make_file, ensure_ascii=False, indent="\t")
+
 
     #df_data = pd.concat([ apt.DataReader(code, args.date) for code in codes ], ignore_index=True)
     #df_data.to_csv("output.csv", encoding='utf-8-sig')
