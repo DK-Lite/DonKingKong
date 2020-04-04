f247258f12fbe0a3fcf2bc604f424ce0ca6c4053
root <root@big-test.asia-northeast1-b.c.striped-rhino-260006.internal>
2020-04-04 05:34:19 +0000

    update find and modify

--- cleanser/apt_trade_info.py
+++ cleanser/apt_trade_info.py
@@ -3,6 +3,7 @@
 # set data
 import re
 from pymongo import MongoClient
+from tqdm import tqdm
 
 DB_HOST="localhost"
 DB_PORT=27017
@@ -23,41 +24,41 @@ def main():
 
     # find
     db = client.data_lake
-    colloction = db.apt_trade_info
+    collection = db.apt_trade_info
 
     data_warehouse = client.data_warehouse
     collection_warehouse = data_warehouse.apt_trade_info
 
     #{ $pop: { 필드1: ±1, 필드2: ±1, ... } }
-    while(True):
-        doc = colloction.find_and_modify({
-            'query' : {'clean':{ '$exists': False }},
+    len = collection.find({'clean':{ '$exists': False}}).count()
+    for i in tqdm(range(len)):
+        doc = collection.find_and_modify(
+            query= {'clean':{ '$exists': False }},
             #'sort'  : {name:1},
-            'remove': False,
-            'update': { "$set": { 'clean' : 1 }},
-            'new'   : False,
+            #'remove': False,
+            update= { "$set": { 'clean' : 1 }},
+            new   = False,
             #'fields': {_id:0,name:1,age:1},
-            'upsert': False,
-            'bypassDocumentValidation': False
-        })
-        print ( doc ) 
+            upsert= False,
+            #'bypassDocumentValidation': False
+        )
         
-        # collection_warehouse.insert_many({
-        #     'tradeValue' : cleanText(doc['거래금액']),
-        #     'buildYear' : doc['건축년도'],
-        #     'lawName' : doc['법정동'],
-        #     'dedicatedArea': doc['전용면적'],
-        #     'aptName' : cleanText(doc['아파트']),
-        #     'tradeYear': doc['년'],
-        #     'tradeMonth': doc['월'],
-        #     'tradeDay': doc['일'],
-        #     'roadCityCode': doc['도로명시군구코드'],
-        #     'lawTownCode': doc['법정동읍면동코드'],
-        #     'roadCode': doc['도로명코드'],
-        #     'roadGroundCode': doc['도로명지상지하코드'],
-        #     'roadMainCode': doc['도로명건물본번호코드'],
-        #     'roadSubCode': doc['도로명건물부번호코드'],
-        # })
+#        collection_warehouse.insert_many({
+#            'tradeValue' : cleanText(doc['거래금액']),
+#            'buildYear' : doc['건축년도'],
+#            'lawName' : doc['법정동'],
+#            'dedicatedArea': doc['전용면적'],
+#            'aptName' : cleanText(doc['아파트']),
+#            'tradeYear': doc['년'],
+#            'tradeMonth': doc['월'],
+#            'tradeDay': doc['일'],
+#            'roadCityCode': doc['도로명시군구코드'],
+#            'lawTownCode': doc['법정동읍면동코드'],
+#            'roadCode': doc['도로명코드'],
+#            'roadGroundCode': doc['도로명지상지하코드'],
+#            'roadMainCode': doc['도로명건물본번호코드'],
+#            'roadSubCode': doc['도로명건물부번호코드'],
+#        })
 
     client.close()
 
